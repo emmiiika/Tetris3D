c286914743c1797ce21f2acf165dba0df13a2c5e
emmiiika <emmikaaaaa@gmail.com>
2025-01-29 21:37:10 +0100

    add movement and rotation classes

---
+++ Assets/Scripts/TetrominoMovement.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class TetrominoMovement : MonoBehaviour{
+
+
+    public void onMoveLeft(){}
+    public void onMoveRight(){}
+    public void onMoveUp(){}
+    public void onMoveDown(){}
+}
---
+++ Assets/Scripts/TetrominoRotation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class TetrominoRotation : MonoBehaviour{
+
+
+    public void onRotateLeft(){}
+    public void onRotateRight(){}
+    public void onRotateUp(){}
+    public void onRotateDown(){}
+}
